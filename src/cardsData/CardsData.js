aa10e34dbc51d541a585bd4a1fb57a465a0e4d74
Alaa mohamed <en-AlaaMohamed>
2024-07-03 20:15:06 +0300

    Upload Changes

--- src/cardsData/CardsData.js
+++ src/cardsData/CardsData.js
@@ -1,10 +1,12 @@
 import grill from '../images/Grill.jpg';
 import fastFood from '../images/Fast-Food.jpg';
+import graduationProject from '../images/graduation-project.png';
 
 
 const cards = [
     { id:0, image: grill, url:"https://eng-alaamohamed.github.io/Restaurant-Grill/"},
-    { id:1, image: fastFood, url:"https://eng-alaamohamed.github.io/Fast-Food/"},
+    { id:1, image: graduationProject, url:"https://graduation-project-frontend-ochre.vercel.app"},
+    { id:2, image: fastFood, url:"https://eng-alaamohamed.github.io/Fast-Food/"},
 ];
 
 
