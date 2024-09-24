961eacae06d998feb075264bc070e54b39929ac4
Alaa mohamed <en-AlaaMohamed>
2024-09-25 01:47:30 +0300

    Upload Changes

--- src/cardsData/CardsData.js
+++ src/cardsData/CardsData.js
@@ -1,12 +1,14 @@
-import grill from '../images/Grill.jpg';
-import fastFood from '../images/Fast-Food.jpg';
 import graduationProject from '../images/graduation-project.png';
+import fastFood from '../images/Fast-Food.jpg';
+import grill from '../images/Grill.jpg';
+import e_commerce_website from '../images/E-commerce Website.png';
 
 
 const cards = [
-    { id:0, image: grill, url:"https://eng-alaamohamed.github.io/Restaurant-Grill/"},
     { id:1, image: graduationProject, url:"https://graduation-project-frontend-ochre.vercel.app"},
+    { id:3, image: e_commerce_website, url:"https://eng-alaamohamed.github.io/E-commerce-website"},
     { id:2, image: fastFood, url:"https://eng-alaamohamed.github.io/Fast-Food/"},
+    { id:0, image: grill, url:"https://eng-alaamohamed.github.io/Restaurant-Grill/"},
 ];
 
 
