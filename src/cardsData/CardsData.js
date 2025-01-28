c8e7fddff2923a837454180d0bf39b0d83e2e0d6
Alaa mohamed <en-AlaaMohamed>
2025-01-28 19:10:24 +0200

    Upload Changes

--- src/cardsData/CardsData.js
+++ src/cardsData/CardsData.js
@@ -7,7 +7,7 @@ import e_commerce_website from '../images/E-commerce Website.png';
 const cards = [
     { id:1, image: graduationProject, url:"https://graduation-project-frontend-ochre.vercel.app"},
     { id:3, image: e_commerce_website, url:"https://eng-alaamohamed.github.io/E-commerce-website"},
-    { id:2, image: fastFood, url:"https://eng-alaamohamed.github.io/Fast-Food/"},
+    { id:2, image: fastFood, url:"https://eng-alaamohamed.github.io/fast-food/"},
     { id:0, image: grill, url:"https://eng-alaamohamed.github.io/Restaurant-Grill/"},
 ];
 
